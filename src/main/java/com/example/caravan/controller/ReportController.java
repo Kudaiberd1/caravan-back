89421addc4e98ab1c54bbb6559277654f1650945
Kudaiberdi <kdajberdiz@gmail.com>
2026-02-23 22:35:29 +0500

    add Report-related classes and services, including ReportController, ReportService, and repositories; implement report retrieval logic

--- src/main/java/com/example/caravan/controller/ReportController.java
+++ src/main/java/com/example/caravan/controller/ReportController.java
@@ -1,6 +1,8 @@
 package com.example.caravan.controller;
 
 import com.example.caravan.dto.response.ReportResponse;
+import com.example.caravan.service.ReportService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,11 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@AllArgsConstructor
 @RequestMapping("/reports")
 public class ReportController {
 
+    private final ReportService reportService;
+
     @GetMapping("/")
     public ResponseEntity<List<ReportResponse>> getReports() {
-
+        List<ReportResponse> reports = reportService.getReports();
+        return ResponseEntity.ok(reports);
     }
 }
